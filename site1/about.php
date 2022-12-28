4ac05500a1fff1f3bdbac1217d461fd4d4dfaed9
bhavin-231 <112623737+bhavin-231@users.noreply.github.com>
2022-12-28 18:04:55 +0530

    Add files via upload

---
+++ site1/about.php
@@ -0,0 +1,10 @@
+<?php 
+include_once('header.php');
+?>
+<div class="container">
+    <div class="raw">
+        <div class="col">
+            thi is a price page
+        </div>
+    </div>
+</div>
