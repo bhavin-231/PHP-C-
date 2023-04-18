21dfaaf190e1c32d3bc59eed000e09096782b961
bhavin-231 <112623737+bhavin-231@users.noreply.github.com>
2023-04-18 11:28:49 +0530

    Add files via upload

---
+++ advance php/advance.php
@@ -0,0 +1,32 @@
+<?php
+trait Trait1
+{
+    public function hello() 
+    {
+        print "hello ";
+    }
+}
+
+trait Trait2
+{
+    public function world() 
+    {
+        print "world";
+    }
+}
+
+class MyClass
+{
+    use Trait1;
+    use Trait2;
+}
+
+$myObj = new MyClass();
+$myObj->hello();
+$myObj->world();
+?>
+
+<?php
+
+
+?>
